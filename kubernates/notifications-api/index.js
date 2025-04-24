97155cc17df955ded51441f3b8ac1015167f29cc
burak.dunal <burak.dunal@gmail.com>
2025-04-24 09:50:50 +0300

    kubernates deployments and configmap

--- kubernates/notifications-api/index.js
+++ kubernates/notifications-api/index.js
@@ -3,14 +3,14 @@ const amqp = require("amqplib");
 
 const app = express();
 
-const accountSid = "?";
-const authToken = "?";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 async function connect() {
   try {
     const connection = await amqp.connect(
-      "amqp://rabbitmq-cluster-ip-service:5672"
+      `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
     );
     const channel = await connection.createChannel();
     const result = channel.assertQueue("jobs");
@@ -19,7 +19,7 @@ async function connect() {
       client.messages
         .create({
           body: "Your order is out for delivery",
-          from: "+18573714765",
+          from: process.env.TWILIO_NUMBER,
           to: message.content.toString(),
         })
         .then((message) => console.log(message.sid))
